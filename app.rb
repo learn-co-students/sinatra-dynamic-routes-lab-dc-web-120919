ba12f8daf85050f20ab562bcbdcedf1caec19a4e
github username <github email address>
2020-01-06 16:01:47 -0500

    pause2

--- app.rb
+++ app.rb
@@ -19,8 +19,15 @@ class App < Sinatra::Base
     @phrase * @number
   end
 
-  get 'say/:word1/:word2/:word3/:word4/:word5' do
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_1
+    @add = num1 + num2 
+
   end
   
 
