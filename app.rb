356349ebbfc53e057d67a1b099c16af75082c28c
github username <github email address>
2020-01-06 15:56:10 -0500

    pause1

--- app.rb
+++ app.rb
@@ -20,13 +20,9 @@ class App < Sinatra::Base
   end
 
   get 'say/:word1/:word2/:word3/:word4/:word5' do
-  @word1 = params[:word1].to_s
-  @word2 = params[:word2].to_s
-  @word3 = params[:word3].to_s
-  @word4 = params[:word5].to_s
-  @word5 = params[:word5].to_s
-  "@word1 + @word2 + @word3 + @word4 + @word5"
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
+  
 
 
 end
